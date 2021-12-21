fbb3bb35eb809ed20ab0d9623a4a6ba3c1298394
emilianoValtech <emiliano.bisurgi@valtech.com>
2021-12-21 15:00:07 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+//Listen for submit
+
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    e.preventDefault()
+    //hide results
+    document.getElementById('results').style.display = 'none'
+    //Show loader
+    document.getElementById('loading').style.display = 'block'
+
+    setTimeout(calculateResults, 2000)
+})
+
+// Calculate results
+function calculateResults(){
+    //UI Vars
+    const amount = document.getElementById('amount')
+    const interest = document.getElementById('interest')
+    const years = document.getElementById('years')
+
+    const monthlyPayment = document.getElementById('monthly-payment')
+    const totalPayment = document.getElementById('total-payment')
+    const totalInterest = document.getElementById('total-interest')
+
+
+    const principal = parseFloat(amount.value)
+    const calculatedInterest = parseFloat(interest.value) / 100 / 20
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //Compute montlhy payment
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+    const monthly = (principal * x * calculatedInterest) / (x - 1)
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2)
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2)
+        totalInterest.value = ((monthly * calculatedPayments) - principal.toFixed(2))
+
+        document.getElementById('results').style.display = 'block'
+        document.getElementById('loading').style.display = 'none'
+    }else {
+        showError('Please check your numbers')
+        document.getElementById('loading').style.display = 'none'
+    }
+
+
+}
+
+// Show error
+function showError(error){
+    //Get elements
+    const card = document.querySelector('.card')
+    const heading = document.querySelector('.heading')
+
+    //Create a div
+    const errorDiv = document.createElement('div')
+    //Add the class
+    errorDiv.className = 'alert alert-danger'
+    //Add text node and append
+    errorDiv.appendChild(document.createTextNode(error))
+    //insert error into the DOM
+    card.insertBefore(errorDiv, heading)
+    setTimeout(clearError, 2000)
+}
+
+function clearError(){
+    document.querySelector('.alert').remove()
+}
+
+
+
+
+
